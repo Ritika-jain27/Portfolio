eb38be049a9491e3f5415722773ec37bd7fbe268
Ritika-jain27 <ritikaj088@gmail.com>
2022-07-14 14:32:00 +0530

    Portfolio

--- form.js
+++ form.js
@@ -1,33 +1,35 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBBW_OUpmNn9omlcYudEGmCQp_2Qpj3dxM",
-    authDomain: "portfolio-form-387ef.firebaseapp.com",
-    projectId: "portfolio-form-387ef",
-    storageBucket: "portfolio-form-387ef.appspot.com",
-    messagingSenderId: "764269230768",
-    appId: "1:764269230768:web:eb794eaa37651a5dd09975"
-  };
-  firebase.initializeApp(firebaseConfig);
-  var firestore=firebase.firestore();
-  const db=firestore.collection("formData");
-  let submitButton=document.getElementById('submit');
-  submitButton.addEventListener("click",(e)=>{
-    e.preventDefault();
-    let Name=document.getElementById('name').value;
-    let Email=document.getElementById('email').value;
-    let Subject=document.getElementById('subject').value;
-    let Textarea=document.getElementById('textarea').value;
+  apiKey: "AIzaSyAVkBH5jAKti3ZGBRg-SW8xTX4pIimSDE0",
+  authDomain: "portfolio-e6935.firebaseapp.com",
+  projectId: "portfolio-e6935",
+  storageBucket: "portfolio-e6935.appspot.com",
+  messagingSenderId: "992216091069",
+  appId: "1:992216091069:web:9b521ba502548ba1faaad4",
+};
 
-    db.doc().set({
-        name:Name,
-        email:Email,
-        subject:Subject,
-        textarea:Textarea
-    }).then(()=>{
-        console.log("Data Saved");
-    }).catch((error)=>{
-          console.log(error);
-    })
-    document.getElementById("Riya-Website").reset();
-    alert("Your form has been submit successfully!");
-   
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+const db = firestore.collection("Ritika");
+let submitButton = document.getElementById('submit');
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let Name = document.getElementById('name').value;
+  let Email = document.getElementById('email').value;
+  let Subject = document.getElementById('subject').value;
+  let Textarea = document.getElementById('textarea').value;
+
+  db.doc().set({
+    name: Name,
+    email: Email,
+    subject: Subject,
+    textarea: Textarea
+  }).then(() => {
+    console.log("Data Saved");
+  }).catch((error) => {
+    console.log(error);
   })
+  document.getElementById("Ritika-Website").reset();
+  alert("Your form has been submit successfully!");
+
+})
+
